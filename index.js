212efeb5fbe42b4c2a70827d37596c55eb75559a
r6cky <cleitonrocha321@hotmail.com>
2025-01-14 09:40:28 -0300

    ex 4, done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1
+// EX 1
 
 let indice = 13;
 let soma = 0;
@@ -9,4 +9,40 @@ while (k < indice) {
   soma += k;
 }
 
-console.log(`O valor de soma é ${soma}`);
+console.log(`O valor de soma é: ${soma}`);
+
+//-----------------------------------------------------------
+
+//EX 2
+
+// EX 3
+
+// EX 4
+
+const calculoPercentual = () => {
+  let faturamento = {
+    sp: 6783643,
+    rj: 3667866,
+    mg: 2922988,
+    es: 2716548,
+  };
+
+  let total = Number(
+    faturamento.sp + faturamento.rj + faturamento.mg + faturamento.es
+  );
+
+  for (key in faturamento) {
+    console.log(
+      key +
+        ": " +
+        faturamento[key] +
+        " - percentual: " +
+        ((faturamento[key] / total) * 100).toFixed(2) +
+        "%"
+    );
+  }
+};
+calculoPercentual();
+// EX 5
+
+const reverse = (value) => {};
